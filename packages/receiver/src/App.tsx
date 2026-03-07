e03784acc51683e53b993944f183ae9ce07ec14e
simplyyliam <simplyliam.dev@gmail.com>
2026-03-07 21:46:38 +0200

    feat(receiver): add setInterval for hot-reloading for incoming urls

--- packages/receiver/src/App.tsx
+++ packages/receiver/src/App.tsx
@@ -2,27 +2,32 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function App() {
-  const [latestResponse, setLatestResponse] = useState("")
-
+  const [latestResponse, setLatestResponse] = useState("");
 
   useEffect(() => {
-
     const fetchLatestUrl = async () => {
       try {
-        const res = await axios.get("http://localhost:8080/url")
-        setLatestResponse(res.data.url)
+        const res = await axios.get("http://localhost:8080/url");
+        setLatestResponse(res.data.url);
       } catch (error) {
-        console.error(error)
-        setLatestResponse("No URL submitted yet or error fetching data")
+        console.error(error);
+        setLatestResponse("No URL submitted yet or error fetching data");
       }
-    }
+    };
 
-    fetchLatestUrl()
-  }, [])
-  
-  return (  
+    fetchLatestUrl();
+
+    const interval = setInterval(fetchLatestUrl, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
     <div className="flex items-center justify-center w-screen h-screen ">
-      <iframe src={`https://${latestResponse}`} className="w-full h-full"></iframe>
+      <iframe
+        src={`https://${latestResponse}`}
+        className="w-full h-full"
+      ></iframe>
     </div>
   );
 }
