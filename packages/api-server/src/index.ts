7613997134f2e51eda45e62bd9d848724dbcd5fa
simplyyliam <simplyliam.dev@gmail.com>
2026-03-07 19:50:54 +0200

    feat(controller + api-server): add frontend axios post method + configured index.ts to send posts to url

--- packages/api-server/src/index.ts
+++ packages/api-server/src/index.ts
@@ -1,10 +1,20 @@
-const express = require("express")
-
+import cors from 'cors';
+import express, { Response, Request } from "express"
 const app = express()
 const PORT = 8080
 
-app.get("/ping", (_req: any, res: { json: (arg0: { message: string }) => void }) => {
+app.use(cors())
+app.use(express.json())
+
+app.get("/ping", (req: Request, res: Response) => {
   res.json({ message: "pong" })
+})
+
+app.post('/url', (req: Request, res: Response) => {
+  const { url } = req.body
+  if (!url) return res.status(400).send({ error: "Missign URL" })
+  console.log("Recieved URL:", url)
+  res.json({ message: `Server received: "${url}"` })
 })
 
 app.listen(PORT, () => {
--- packages/controller/src/App.tsx
+++ packages/controller/src/App.tsx
@@ -1,7 +1,40 @@
+import axios from "axios";
+import { useState, type SyntheticEvent } from "react";
+
 export default function App() {
+  const [input, setInput] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("http://localhost:8080/url", {
+        url: input,
+      });
+      setResponse(res.data.message);
+    } catch (error) {
+      console.error(error);
+      setResponse("Error sending data to server");
+    }
+  };
+
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      Hello Controller
+    <div className="flex flex-col gap-2 items-center justify-center w-screen h-screen">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="border p-2"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Enter URL..."
+        />
+        <button type="submit">Submit</button>
+      </form>
+
+      <div className="flex items-center justify-center w-10 h-5 ">
+        {response}
+      </div>
     </div>
   );
 }
