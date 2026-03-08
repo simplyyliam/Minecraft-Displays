3f11c442646579103977b1a21100fdb39a5cd0fb
simplyyliam <simplyliam.dev@gmail.com>
2026-03-08 13:15:28 +0200

    removed dist folders

--- packages/ui/dist/src/input.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export default function Input() {
-    return (_jsx("div", { className: "flex items-center justify-center", children: _jsx("input", { type: "text" }) }));
-}
