d0fb2eae25016c8ba9488614c7132658c58294d8
simplyyliam <simplyliam.dev@gmail.com>
2026-03-08 19:52:06 +0200

    feat(lobby + rooms): rooms and lobby
    - add room GET logic to retrieve rooms from controller
    
     - add rooms to lobby

--- packages/cinema/src/lobby/lobby.tsx
+++ packages/cinema/src/lobby/lobby.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Lobby() {
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      Hello Lobby
+    <div className="flex flex-col gap-2 items-center justify-center w-screen h-screen">
+      <Link to="/room/1">Room 1</Link>
+      <Link to="/room/2">Room 2</Link>
+      <Link to="/room/3">Room 3</Link>
+      <Link to="/room/4">Room 4</Link>
     </div>
   );
 }
--- packages/cinema/src/rooms/rooms.tsx
+++ packages/cinema/src/rooms/rooms.tsx
@@ -1,9 +1,39 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { API_URL } from "../constants";
 
 export default function Rooms() {
-    return (
-        <div className="flex">
-            Room
-        </div>
-    )
+  const { roomId } = useParams();
+  const [roomUrl, setRoomUrl] = useState("");
+
+  useEffect(() => {
+    const fetchRooms = async () => {
+      try {
+        const res = await axios.get(`${API_URL}/url/${roomId}`);
+        setRoomUrl(res.data);
+      } catch (error) {
+        console.error(`Could not find ${roomId}:`, error);
+      }
+    };
+
+    fetchRooms();
+  }, [roomId]);
+
+  return (
+    <div className="flex items-center justify-center w-screen h-screen">
+      {roomUrl ? (
+        <iframe
+          key={roomUrl}
+          src={roomUrl}
+          allowFullScreen
+          allow="fullscreen *; encrypted-media; clipboard-read; clipboard-write; camera; microphone"
+          title={roomId}
+          className="w-full h-full border-none"
+        />
+      ): (
+        <p>Waiting For URL...</p>
+      )}
+    </div>
+  );
 }
